235a12e69434d8f19106d1ab331e891e13b9ea66
Tata <tatasamy1998@gmail.com>
2020-08-31 23:21:36 +0200

    add home

--- src/controller/home.java
+++ src/controller/home.java
@@ -26,8 +26,10 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -40,12 +42,45 @@ import service.DB;
  */
 public class home implements Initializable {
 
-    @FXML
-    private Label label;
+        @FXML
+    private Label l_g;
     @FXML
     private VBox pnItems;
     @FXML
     private HBox itemC;
+    
+     @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnOrders;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnMenus;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private Pane pnlCustomer;
+
+    @FXML
+    private Pane pnlOrders;
+
+    @FXML
+    private Pane pnlMenus;
+
+    @FXML
+    private Pane pnlOverview;
 
     @FXML
     private void close(ActionEvent event) {
@@ -79,6 +114,7 @@ public class home implements Initializable {
             allDb.DB_connection();
             t = allDb.over(day);
             allDb.DB_close();
+            l_g.setText(t.size()+"");
 
             for (int i = 0; i < t.size(); i++) {
                 buildover(t.get(i));
@@ -104,24 +140,51 @@ public class home implements Initializable {
         l4.setTextFill(Color.WHITE);
         l5.setTextFill(Color.WHITE);
 
-        l1.setPrefSize(80, 20);
-        l2.setPrefSize(50, 20);
-        l3.setPrefSize(20, 20);
-        l4.setPrefSize(40, 20);
-        l5.setPrefSize(80, 20);
+        l1.setPrefSize(80, 50);
+        l2.setPrefSize(50, 50);
+        l3.setPrefSize(20, 50);
+        l4.setPrefSize(40, 50);
+        l5.setPrefSize(80, 50);
         
         HBox h = new HBox();
-        h.setPrefSize(600, 53);
+        h.setPrefSize(600, 300);
         h.setAlignment(Pos.CENTER_LEFT);
         h.setSpacing(80);
-        h.setStyle("-fx-background-color: #000000; -fx-background-radius: 5; -fx-background-insets: 0;");
+        h.setStyle("-fx-background-color : #02030A");
 
+          h.setOnMouseEntered(event -> {
+                    h.setStyle("-fx-background-color : #0A0E3F");
+                });
+                h.setOnMouseExited(event -> {
+                    h.setStyle("-fx-background-color : #02030A");
+                });
         h.getChildren().add(l1);
         h.getChildren().add(l2);
         h.getChildren().add(l3);
         h.getChildren().add(l4);
         h.getChildren().add(l5);
         pnItems.getChildren().add(h);
+    }
+
+
+   public void swi(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnCustomers) {
+            pnlCustomer.setStyle("-fx-background-color : #1620A1");
+            pnlCustomer.toFront();
+        }
+        if (actionEvent.getSource() == btnMenus) {
+            pnlMenus.setStyle("-fx-background-color : #53639F");
+            pnlMenus.toFront();
+        }
+        if (actionEvent.getSource() == btnOverview) {
+            pnlOverview.setStyle("-fx-background-color : #02030A");
+            pnlOverview.toFront();
+        }
+        if(actionEvent.getSource()==btnOrders)
+        {
+            pnlOrders.setStyle("-fx-background-color : #464F67");
+            pnlOrders.toFront();
+        }
     }
 
 }
